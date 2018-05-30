53f4773917c1c6514ac6ae289893dbff45cf04d5
Kexin Zhang <kxin.zhang@gmail.com>
2018-05-30 12:45:47 -0700

    take out mailchimp css, switch paragraph fonts, add link to org site

--- js/countdown.js
+++ js/countdown.js
@@ -25,34 +25,16 @@ updateDate();
 setInterval(updateDate, 60 * 1000);
 
 // for mailchimp mailing list subscription pop up
-function clear() {
-    document.getElementById('modal').style.display = 'none';
-
-    // remove validation stuff
-    var errorMessage = document.getElementById('mce-error-response');
-    var successMessage = document.getElementById('mce-success-response');
-    errorMessage.textContent = '';
-    errorMessage.style.display = 'none';
-    successMessage.textContent = '';
-    successMessage.style.display = 'none';
-
-    var messages = document.querySelectorAll('div.mce_inline_error');
-    for (var i = 0; i < messages.length; i++) {
-        messages[i].textContent = '';
-    }
-    document.getElementById('mce-EMAIL').classList.remove('mce_inline_error');
-}
-
 document.getElementById('subscribe').onclick = function() {
     document.getElementById('modal').style.display = 'block';
 }
 
 document.getElementById('modal').onclick = function(event) {
     if(event.target.id == 'modal') {
-        clear();
+        document.getElementById('modal').style.display = 'none';
     }
 }
 
 document.getElementById('close').onclick = function() {
-    clear();
+    document.getElementById('modal').style.display = 'none';
 }
